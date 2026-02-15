a0f1b0d668f0c54a5d2670db5f0c04fd2c6fa550
konstantinkovalev <konstkov@metropolia.fi>
2026-02-15 14:14:01 +0200

    Parsing and outputting C-style character array using subroutines

---
+++ lab-4-subroutines/main-1.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <string.h>
+
+__attribute__(( naked )) int prt(const char *a)
+{
+	// the pointer to a character array is passed to the
+	// subroutine in R0
+	// remember to follow AAPCS:
+	// push any register above r3 at the beginning and pop the same registers at the end
+	// note: lr needs to be pushed because we are calling another subroutine
+    // cortex-M0 requires popping to PC if LR was pushed. See the code below
+		asm volatile
+	(
+			"push { r5, r6, r7, lr } \n" // we need to save return address because we call another subroutine
+
+			// Register Mapping:
+
+			// r0, original pointer, also used by putchar for printing the character
+			// r5: holds the pointer the copy of pointer from r0 for incrementing it
+			// r6: stores the value of dereferenced pointer
+			// r7: keeps the count of characters
+
+			"mov r5, r0 \n" // copy the pointer from r0 and store it in r5
+
+			"repeat: \n" // repeat the procedure
+			"ldrb r6, [r5] \n" // dereference the pointer from r5 and store it in r6 (it is an unsigned byte)
+
+			"cmp r6, #0 \n" // compare the value stored in r6 to '0' ( in order to detect terminating null)
+			"beq out \n" // if it equals 0 - jump to 'out' branch
+
+			"add r7, r7, #1 \n" // increment character counter by 1 and store it in r7 (don't count the terminating null)
+
+			"compare_to_A: \n" // 'branch' compare
+			"cmp r6, #64 \n" // compare the value from r6 to 64 (65 is ASCII code of first capital letter - 'A')
+			"bhi compare_to_Z \n" // if it is unsigned (it is according to the assignment) and higher - jump to 'compare_to_Z' branch
+			"b print \n" // otherwise jump to print branch
+
+			"compare_to_Z: \n" // 'compare_to_Z' branch
+			"cmp r6, #90 \n" // compare the value from r6 to '90' (ASCII code for last capital letter - 'Z')
+			"bls lowercase\n" // if it is less or equal - jump to branch lowercase
+			"b print \n" // otherwise jump to branch print
+
+			"lowercase: \n" // branch 'lowercase'
+			"add r6, r6, #32\n" // 32 is the difference in ASCII table between capital and lowercase letters
+			"b print \n" // jump to branch 'print'
+
+			"print: \n" // 'print' branch
+			"add r5, r5, #1 \n" // increment the pointer by 1 ( because char is 1 byte)
+			"mov r0, r6 \n" // copy the value stored in r2 to r0 (putchar uses r0 for printing characters)
+			"bl putchar \n"
+			"b repeat \n" // repeat the cycle
+
+			// to return a value copy it ro R0 before returning from subroutine
+
+			"out: \n" // pop back the registers and end the program
+			"mov r0, r7 \n" // copy the character count from r7 and store in r0
+			"pop { r5, r6, r7, pc } \n" // cortex-M0 requires popping to PC if LR was pushed
+            // popping to PC will cause return from subroutine (~same as "bx lr")
+	);
+}
+
+void fail() {
+    printf("Failed\n"); // set a break point here
+    while(1) {
+        tight_loop_contents();
+    }
+}
+
+void ok() {
+    printf("\nAll ok\n"); // set a break point here
+}
+
+int main(void) {
+
+    const uint led_pin = 22;
+    int len = 0;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+	// TODO: insert code here
+	printf("\nExercise5\n");
+
+	char test1[] = "Computer Architecture\n";
+	const char test2[] = "Computer Architecture\n";
+	len = prt(test1);
+	if(strcmp(test1, test2)) {
+		fail(); // error - string modified
+	}
+	if(len != 22) {
+		fail(); // error - incorrect length returned
+	}
+	char test3[] = "Johnny Ca$h:Live @Folsom\n";
+	const char test4[] = "Johnny Ca$h:Live @Folsom\n";
+	len = prt(test3);
+	if(strcmp(test3, test4)) {
+        fail(); // error - string modified
+	}
+	if(len != 25) {
+		fail(); // error - incorrect length returned
+	}
+
+	char test5[] = "If you like to gamble, I tell you I'm your man\n";
+	const char test6[] = "If you like to gamble, I tell you I'm your man\n";
+	len = prt(test5);
+	if(strcmp(test5, test6)) {
+        fail(); // error - string modified
+	}
+	if(len != 47) {
+		fail(); // error - incorrect length returned
+	}
+
+    ok();
+
+    // Loop forever and blink an LED to indicaste success
+    while (true) {
+        // Blink LED
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
+	return 0 ;
+}
